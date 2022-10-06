04e8803ece7eb5f6eed304e6744d12861199e707
Paulodhiambopy <115166908+Paulodhiambopy@users.noreply.github.com>
2022-10-06 17:18:46 +0300

    Add files via upload

---
+++ ordinal_numbers.py
@@ -0,0 +1,25 @@
+ordinal_numbers = list(range(1,10))
+for ordinal_number in ordinal_numbers:
+    if ordinal_number ==1:
+        print("1st")
+    elif ordinal_number ==2:
+        print("2nd")
+    elif ordinal_number ==3:
+        print("3rd")
+    elif ordinal_number ==4:
+        print("4th")
+    elif ordinal_number ==5:
+        print("5th")
+    elif ordinal_number ==6:
+        print("6th")
+    elif ordinal_number ==7:
+        print("7th")
+    elif ordinal_number ==8:
+        print("8th")
+    elif ordinal_number  == 9:
+        print("9th")
+    
+    
+    
+
+ 
